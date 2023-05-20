bbce2c4781ee6a66618f13957f846f7dcf14d748
Steven Krenn <stevekrenn@gmail.com>
2023-05-20 15:35:59 -0500

    added container sass stylings

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
+import './categories.styles.scss'
+
 const App = () => {
 
   const categories = [
     {
       id: 1,
       title: 'Hats'
-    }
+    },
+    {
+      id: 2,
+      title: 'Jackets'
+    },
+    {
+      id: 3,
+      title: 'Sneakers'
+    },
+    {
+      id: 4,
+      title: 'Womens'
+    },
+    {
+      id: 5,
+      title: 'Mens'
+    },
   ]
 
   return (
     <div className='categories-container'>
       {categories.map(({ title }) => (
         <div className='category-container'>
-          {/* <img/> */}
+          <div className='background-image' />
           <div className='category-body-container'>
             <h2>{title}</h2>
             <p>Shop Now</p>
