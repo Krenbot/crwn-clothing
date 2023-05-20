d637d340c4fb96a5a251ba0783ae4b566e80ab8a
Steven Krenn <stevekrenn@gmail.com>
2023-05-20 12:16:11 -0500

    mapped over categories in app.js from inital scaffolding

--- src/App.js
+++ src/App.js
@@ -1,43 +1,25 @@
 const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats'
+    }
+  ]
+
   return (
-    <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now!</p>
+    <div className='categories-container'>
+      {categories.map(({ title }) => (
+        <div className='category-container'>
+          {/* <img/> */}
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now!</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now!</p>
-        </div>
-      </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
