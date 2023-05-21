42bfdbb23cd872f7888ddde2930acdc869716d4d
Steven Krenn <stevekrenn@gmail.com>
2023-05-21 12:26:26 -0500

    added nav logo, nav scss

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,11 +1,12 @@
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
 const Navigation = () => {
     return (
         <>
             <div className='navigation'>
                 <Link className='logo-container' to='/'>
-                    <div>Logo</div>
+                    <CrwnLogo className='logo' />
                 </Link>
                 <div className='nav-links-container'>
                     <Link className='nav-link' to='/shop'>
