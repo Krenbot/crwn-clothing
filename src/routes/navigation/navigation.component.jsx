624bf6a721fd4b2345ee13f244d1be0c5e83dee4
Steven Krenn <stevekrenn@gmail.com>
2023-05-21 12:27:49 -0500

    correctly imported nav scss

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
         <>
