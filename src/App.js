0fafe4bf0cbd8e5299a06fe4ef6fe6dc948aa9e1
Steven Krenn <stevekrenn@gmail.com>
2023-05-21 08:54:13 -0500

    added browserRouter

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ const App = () => {
   const categories = [
     {
       id: 1,
-      title: 'Hats',
+      title: 'HATS',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
-      title: 'Jackets',
+      title: 'JACKETS',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: 'SNEAKERS',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
-      title: 'Womens',
+      title: 'WOMENS',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
-      title: 'Mens',
+      title: 'MENS',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ];
