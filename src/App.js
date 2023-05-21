7ea2191bc99e0c17fa0cf9bc50c239098ef23a8a
Steven Krenn <stevekrenn@gmail.com>
2023-05-21 09:36:24 -0500

    initalized routes in app, added home to /

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component'
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'HATS',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'JACKETS',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'SNEAKERS',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'WOMENS',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'MENS',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}>
+      </Route>
+    </Routes>
+  )
 };
 
 export default App;
