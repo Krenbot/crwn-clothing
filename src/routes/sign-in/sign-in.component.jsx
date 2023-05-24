5af7ab144f198336dde856029a91c023f1523e17
Steven Krenn <stevekrenn@gmail.com>
2023-05-23 19:15:52 -0500

    fixed typo in firebase utils preventing data collection

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,19 +1,20 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import {
+    signInWithGooglePopup,
+    createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
     const logGoogleUser = async () => {
-        const response = await signInWithGooglePopup();
-        createUserDocumentFromAuth(response)
+        const { user } = await signInWithGooglePopup();
+        const userDocRef = await createUserDocumentFromAuth(user);
     };
 
     return (
-        <>
+        <div>
             <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
-                Sign in with Google
-            </button>
-        </>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        </div>
     );
 };
 
-export default SignIn
+export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,7 +4,7 @@ import {
     getAuth,
     signInWithRedirect,
     signInWithPopup,
-    GoogleAuthProvider
+    GoogleAuthProvider,
 } from 'firebase/auth';
 
 import {
@@ -30,23 +30,23 @@ const firebaseApp = initializeApp(firebaseConfig);
 const provider = new GoogleAuthProvider();
 
 provider.setCustomParameters({
-    prompt: 'select_account'
+    prompt: 'select_account',
 });
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-export const db = (getFirestore);
+
+export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
 
     const userSnapshot = await getDoc(userDocRef);
 
-    //if user data does not exist
     if (!userSnapshot.exists()) {
         const { displayName, email } = userAuth;
         const createdAt = new Date();
-        //create /set the document with the data from userAuth in my collection
+
         try {
             await setDoc(userDocRef, {
                 displayName,
@@ -57,7 +57,6 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             console.log('error creating the user', error.message);
         }
     }
-    //if user data exists
+
     return userDocRef;
-    //return userDocRef
 };
