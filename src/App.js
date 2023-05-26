b223520f48aec4b61863fc80f552803f8647209c
Steven Krenn <stevekrenn@gmail.com>
2023-05-26 10:06:40 -0500

    links to sign-in now correctly changed to auth

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
         <Route path='/shop' element={<Shop />} />
-        <Route path='/authentication' element={<Authentication />} />
+        <Route path='/auth' element={<Authentication />} />
       </Route>
     </Routes>
   )
