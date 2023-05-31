c2755681ca9dab5874e2967deaa9cf6ca496d91b
Steven Krenn <stevekrenn@gmail.com>
2023-05-30 20:26:28 -0500

    intergrated sign out functions with firebase utils

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 import './navigation.styles.scss';
 
 const Navigation = () => {
@@ -19,7 +20,7 @@ const Navigation = () => {
                     </Link>
 
                     {currentUser ? (
-                        <span className='nav-link'>SIGN OUT</span>
+                        <span className='nav-link' onClick={signOutUser}>SIGN OUT</span>
                     ) : (
                         <Link className='nav-link' to='/auth'>
                             SIGN IN
