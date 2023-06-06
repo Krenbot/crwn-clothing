3b92402908ba8a47550852027ade68c7c3a24266
Steven Krenn <stevekrenn@gmail.com>
2023-06-06 11:37:30 -0500

    products and shop route intergrated, shop stylings added

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductsProvider } from './contexts/products.context';
 
 import './index.scss';
 
@@ -11,9 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
+
     </BrowserRouter>
   </React.StrictMode>
 );
