368a2a49a3855749820b5d338c36d10dc70a9527
Steven Krenn <stevekrenn@gmail.com>
2023-06-08 11:01:34 -0500

    adjusted cart dropdown stylings to prevent CHECKOUT word overflow

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,12 @@
-import Button from '../button/button.component'
-import '../cart-dropdown/cart-dropdown.styles.scss'
+import React from 'react';
+import Button from '../button/button.component';
+import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => {
-    return (
-        <div className='cart-dropdown-container'>
-            <div className='cart-items' />
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    )
-}
+const CartDropdown = () => (
+    <div className='cart-dropdown-container'>
+        <div className='cart-items' />
+        <Button>GO TO CHECKOUT</Button>
+    </div>
+);
 
 export default CartDropdown;
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -5,9 +5,9 @@ const CartIcon = () => {
     return (
         <div className='cart-icon-container'>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>10</span>
+            <span className='item-count'>0</span>
         </div>
     )
 }
 
-export default CartIcon
+export default CartIcon;
