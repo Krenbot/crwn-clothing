7d30d7f0420a4880d1ad61a4e509f18ac239bde8
Steven Krenn <stevekrenn@gmail.com>
2023-06-09 21:22:43 -0500

    fixed handling error in cart context. cart dropdown now correctly displays values

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,17 +1,26 @@
-import './product-card.styles.scss'
-import Button from '../button/button.component'
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+import Button from '../button/button.component';
+import './product-card.styles.scss';
 
 const ProductCard = ({ product }) => {
     const { name, price, imageUrl } = product;
+    const { addItemToCart } = useContext(CartContext);
 
-    return (< div className='product-card-container' >
-        <img src={imageUrl} alt={`${name}`} />
-        <div className='footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
+    const addProductToCart = () => addItemToCart(product);
+
+    return (
+        <div className='product-card-container'>
+            <img src={imageUrl} alt={`${name}`} />
+            <div className='footer'>
+                <span className='name'>{name}</span>
+                <span className='price'>{price}</span>
+            </div>
+            <Button buttonType='inverted' onClick={addProductToCart}>
+                Add to card
+            </Button>
         </div>
-        <Button buttonType='inverted'>Add To Cart</Button>
-    </div >)
-}
+    );
+};
 
-export default ProductCard
+export default ProductCard;
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,37 +1,39 @@
 import { createContext, useState } from 'react';
 
-const addCartItem = (cartItems, productToAdd) => {
+export const addCartItem = (cartItems, productToAdd) => {
     //find if cartItems contains productToAdd
-    const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+
     //if found, increment quantity
     if (existingCartItem) {
-        return cartItems.map((cartItem) => cartItem.id === productToAdd.id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
+        return cartItems.map((cartItem) =>
+            cartItem.id === productToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem
         );
     }
+
     //return new array with modified cartItems/new cart item
-    return [...cartItems, { productToAdd, quantity: 1 }]
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
 };
 
 export const CartContext = createContext({
     isCartOpen: false,
     setIsOpen: () => { },
     cartItems: [],
-    addItemToCart: () => { }
+    addItemToCart: () => { },
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
 
-    const addItemToCart = (productToAdd) => {
-        setCartItems(addCartItem(cartItems, productToAdd))
-    };
+    const addItemToCart = (product) =>
+        setCartItems(addCartItem(cartItems, product));
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart };
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
 
-    return (
-        <CartContext.Provider value={value}>{children}</CartContext.Provider>
-    );
+    return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
