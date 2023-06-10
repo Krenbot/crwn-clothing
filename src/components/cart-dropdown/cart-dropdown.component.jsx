74b5fac0896254adefa5f0307bcb42e407603015
Steven Krenn <stevekrenn@gmail.com>
2023-06-09 19:12:30 -0500

    added logic for increasing/decreasing cart item values

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => (
     <div className='cart-dropdown-container'>
-        <div className='cart-items' />
+        <div className='cart-items'>
+
+            {[].map(item => <CartItem cartItem={item} />)}
+
+        </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
 );
