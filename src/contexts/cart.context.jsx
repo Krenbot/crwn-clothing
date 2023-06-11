5f8f459b3f3c75e0466d13aa35474ca596df42de
Steven Krenn <stevekrenn@gmail.com>
2023-06-11 10:55:33 -0500

    implemented cart icon addition logic, fixed typo on ADD TO CART

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const addCartItem = (cartItems, productToAdd) => {
     //find if cartItems contains productToAdd
@@ -23,17 +23,25 @@ export const CartContext = createContext({
     isCartOpen: false,
     setIsOpen: () => { },
     cartItems: [],
+    cartCount: 0,
     addItemToCart: () => { },
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const count = cartItems.reduce(
+            (total, cartItem) => total + cartItem.quantity, 0
+        ); setCartCount(count);
+    }, [cartItems]);
 
     const addItemToCart = (product) =>
         setCartItems(addCartItem(cartItems, product));
 
-    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
