d5bf3b37dc1266c50c08134a12877bc1a929cf18
Steven Krenn <stevekrenn@gmail.com>
2023-06-11 17:00:55 -0500

    added skeleton checkout route

---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,13 @@
+import './checkout.styles.scss'
+
+const Checkout = () => {
+
+
+    return (
+        <div>
+            <h1>I AM THE CHECKOUT PAGE</h1>
+        </div>
+    )
+}
+
+export default Checkout
