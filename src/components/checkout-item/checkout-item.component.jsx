7ad1ff9189badf71e32b4777506f967478eadd17
Steven Krenn <stevekrenn@gmail.com>
2023-06-12 09:29:34 -0500

    add/remove buttons on checkout operational

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -5,8 +5,12 @@ import './checkout-item.styles.scss';
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
-    const { clearItemFromCart } = useContext(CartContext);
+
+    const { clearItemFromCart, addItemToCart, removeItemToCart } = useContext(CartContext);
+
     const clearItemHandler = () => clearItemFromCart(cartItem);
+    const addItemHandler = () => addItemToCart(cartItem);
+    const removeItemHandler = () => removeItemToCart(cartItem);
 
     return (
         <div className='checkout-item-container'>
@@ -14,7 +18,11 @@ const CheckoutItem = ({ cartItem }) => {
                 <img src={imageUrl} alt={`${name}`} />
             </div>
             <span className='name'>{name}</span>
-            <span className='quantity'>{quantity}</span>
+            <span className='quantity'>
+                <div className='arrow' onClick={removeItemHandler}>&#10094;</div>
+                <span className='value'>{quantity}</span>
+                <div className='arrow' onClick={addItemHandler}>&#10095;</div>
+            </span>
             <span className='price'>{price}</span>
             <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
         </div>
