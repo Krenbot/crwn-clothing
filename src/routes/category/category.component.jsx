f7a7c5a8fa244389f8fdad36560c875c42d34561
Steven Krenn <stevekrenn@gmail.com>
2023-06-13 13:50:26 -0500

    category route skeleton showing correctly - fixed syntax issues in category route stylings

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,28 +1,31 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { CategoriesContext } from '../../contexts/categories.context';
 import ProductCard from '../../components/product-card/product-card.component';
 
-import '../category/category.styles.scss';
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import './category.styles.scss';
 
 const Category = () => {
     const { category } = useParams();
     const { categoriesMap } = useContext(CategoriesContext);
-    const [products, setProducts] = useState(categoriesMap(category));
+    const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category]);
-    }, [category, categoriesMap])
+    }, [category, categoriesMap]);
 
     return (
-        <div className='category-container'>
-            {products &&
-                products.map((product) => (
-                    <ProductCard key={product.id} product={product} />
-                ))}
-        </div>
-    )
+        <>
+            <div className='category-container'>
+                {products &&
+                    products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+            </div>
+        </>
+    );
 };
 
 export default Category;
