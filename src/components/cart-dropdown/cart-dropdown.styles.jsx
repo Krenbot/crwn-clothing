a33093e57c27b0c414e914df787dd948af82c440
Steven Krenn <stevekrenn@gmail.com>
2023-06-15 10:44:27 -0500

    changed cart-dropdown to styled components

---
+++ src/components/cart-dropdown/cart-dropdown.styles.jsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+
+import Button from '../button/button.component';
+
+export const CartDropdownContainer = styled.div`
+  position: absolute;
+  width: 240px;
+  height: 340px;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  border: 1px solid black;
+  background-color: white;
+  top: 90px;
+  right: 40px;
+  z-index: 5;
+
+  ${Button} {
+    margin-top: auto;
+  }
+`;
+
+export const EmptyMessage = styled.span`
+  font-size: 18px;
+  margin: 50px auto;
+`;
+
+export const CartItems = styled.div`
+  height: 240px;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+`;
+
+// Original SCSS below
+//     .cart - dropdown - container {
+//     position: absolute;
+//     width: 300px; // Changed from 240px to have the button all on one line
+//     height: 340px;
+//     display: flex;
+//     flex - direction: column;
+//     padding: 20px;
+//     border: 1px solid black;
+//     background - color: white;
+//     top: 90px;
+//     right: 40px;
+//     z - index: 5;
+
+//     .empty - message {
+//         font - size: 18px;
+//         margin: 50px auto;
+//     }
+
+//     .cart - items {
+//         height: 100 %; // Filled the height to match the container and fix button spacing
+//         display: flex;
+//         flex - direction: column;
+//         overflow: scroll;
+//     }
+
+//     button {
+//         margin - top: auto;
+//     }
+// }
+
+// :: -webkit - scrollbar {
+//     // Remove scrollbar
+//     display: none;
+// }
