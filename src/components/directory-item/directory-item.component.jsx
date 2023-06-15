d78ad2eabae7c2a9186fffb7342127c6a7e292aa
Steven Krenn <stevekrenn@gmail.com>
2023-06-15 12:09:01 -0500

    fixed syntax errors in directory items when changing to styled components, error in tabbing

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,19 +1,20 @@
-import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles.jsx'
+import {
+    BackgroundImage,
+    Body,
+    DirectoryItemContainer,
+} from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
     const { imageUrl, title } = category;
-
     return (
         <DirectoryItemContainer>
-            <BackgroundImage
-                imageUrl={imageUrl}
-            />
+            <BackgroundImage imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>
                 <p>Shop Now</p>
             </Body>
         </DirectoryItemContainer>
     );
-}
+};
 
-export default DirectoryItem
+export default DirectoryItem;
--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const BackgroundImage = styled.div`
-width: 100 %;
-height: 100 %;
-background - size: cover;
-background - position: center;
-background-image: ${({ imageUrl }) => `url(${imageUrl})`}
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
 
 export const Body = styled.div`
@@ -35,36 +35,36 @@ export const Body = styled.div`
 `;
 
 export const DirectoryItemContainer = styled.div`
-min-width: 30%;
-height: 240px;
-flex: 1 1 auto;
-display: flex;
-align-items: center;
-justify-content: center;
-border: 1px solid black;
-margin: 0 7.5px 15px;
-overflow: hidden;
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
 
-&:hover {
-  cursor: pointer;
+  &:hover {
+    cursor: pointer;
 
-  & ${BackgroundImage} {
-    transform: scale(1.1);
-    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    & ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+
+    & ${Body} {
+      opacity: 0.9;
+    }
   }
 
-  & ${Body} {
-    opacity: 0.9;
+  &:first-child {
+    margin-right: 7.5px;
   }
-}
 
-&:first-child {
-  margin-right: 7.5px;
-}
-
-&:last-child {
-  margin-left: 7.5px;
-}
+  &:last-child {
+    margin-left: 7.5px;
+  }
 `;
 
 // Original SCSS
