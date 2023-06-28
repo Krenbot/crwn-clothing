17b6d1d5908c8e3697b25a1751410ba7ab36ca29
Steven Krenn <stevekrenn@gmail.com>
2023-06-28 17:24:04 -0500

    applied redux persist

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import './index.scss';
 
@@ -13,10 +14,12 @@ root.render(
 
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
 
 );
