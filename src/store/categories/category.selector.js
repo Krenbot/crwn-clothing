b6486a61d57d5d3677fd15f73a2f4e8b95b7d9a2
Steven Krenn <stevekrenn@gmail.com>
2023-06-28 10:43:59 -0500

    added custom tracking middleware to reducer store, minor formatting

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,7 @@
-export const selectCategoriesMap = (state) => state.categories.categories
-    .reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {})
+export const selectCategoriesMap = (state) =>
+    state.categories.categories
+        .reduce((acc, category) => {
+            const { title, items } = category;
+            acc[title.toLowerCase()] = items;
+            return acc;
+        }, {})
