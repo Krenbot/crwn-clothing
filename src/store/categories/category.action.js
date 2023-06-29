f70e51a275903aa21d0d8a762965cdd9f47affa6
Steven Krenn <stevekrenn@gmail.com>
2023-06-29 12:14:08 -0500

    fetch categories async - thunk to saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,7 +1,6 @@
 import { CATEGORIES_ACTION_TYPES } from './category.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
-
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+// import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
 export const fetchCategoriesStart = () =>
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -15,14 +14,15 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 export const fetchCategoriesFailure = (error) =>
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
 
-export const fetchCategoriesStartAsync = () => {
-    return async (dispatch) => {
-        dispatch(fetchCategoriesStart());
-        try {
-            const categoriesArray = await getCategoriesAndDocuments('categories');
-            dispatch(fetchCategoriesSuccess(categoriesArray));
-        } catch (error) {
-            dispatch(fetchCategoriesFailure(error));
-        }
-    };
-};
+//THUNK ACTION
+// export const fetchCategoriesStartAsync = () => {
+//     return async (dispatch) => {
+//         dispatch(fetchCategoriesStart());
+//         try {
+//             const categoriesArray = await getCategoriesAndDocuments('categories');
+//             dispatch(fetchCategoriesSuccess(categoriesArray));
+//         } catch (error) {
+//             dispatch(fetchCategoriesFailure(error));
+//         }
+//     };
+// };
