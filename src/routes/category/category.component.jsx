485b006bbe908ea3849061fbb4739bb10e68cd43
Steven Krenn <stevekrenn@gmail.com>
2023-06-29 09:47:04 -0500

    removed unnecessary fragment import

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Fragment } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
