e014660c6448eb62e676d563ef78c5a71799d4fe
Steven Krenn <stevekrenn@gmail.com>
2023-06-29 09:51:31 -0500

    whitelisted cart in persist config to show component only when fully loaded

--- src/store/store.js
+++ src/store/store.js
@@ -20,7 +20,7 @@ const composeEnhancer =
 const persistConfig = {
     key: 'root',
     storage,
-    blacklist: ['user'],
+    whitelist: ['cart']
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
