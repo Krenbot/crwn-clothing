c307df616c3546549e84fbbdbe4701d7e6d2737b
Steven Krenn <stevekrenn@gmail.com>
2023-07-06 15:35:08 -0500

    migrated category to typescript. NOTE - added noImplicitAny: false to squash bug

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,22 +1,37 @@
-import { CATEGORIES_ACTION_TYPES, Category, CategoryItem } from './category.types';
+import { CATEGORIES_ACTION_TYPES, Category } from './category.types';
 
-import { createAction, Action, ActionWithPayload } from '../../utils/reducer/reducer.utils';
+import {
+    createAction,
+    Action,
+    withMatcher,
+    ActionWithPayload,
+} from '../../utils/reducer/reducer.utils';
 
+export type FetchCategoriesStart =
+    Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
 
-export type FetchCategoriesStart = Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>
+export type FetchCategoriesSuccess = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+    Category[]
+>;
 
-export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, Category[]>;
+export type FetchCategoriesFailed = ActionWithPayload<
+    CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+    Error
+>;
 
-export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>
+export const fetchCategoriesStart = withMatcher(() =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
+);
 
-export const fetchCategoriesStart = (): FetchCategoriesStart =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesSuccess = withMatcher(
+    (categoriesArray: Category[]) =>
+        createAction(
+            CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+            categoriesArray
+        )
+);
 
-export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
-    createAction(
-        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
-        categoriesArray
-    );
-
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher((error: Error) =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error)
+);
