bc5bda147c1ce8d901e26d7624e75ab40258aa16
Steven Krenn <stevekrenn@gmail.com>
2023-07-06 18:49:06 -0500

    cart selector correctly migrated, minor formatting

--- src/store/cart/cart.selector.ts
+++ src/store/cart/cart.selector.ts
@@ -1,6 +1,8 @@
 import { createSelector } from 'reselect';
 
-const selectCartReducer = (state) => state.cart;
+import { CartState } from './cart.reducer';
+
+const selectCartReducer = (state): CartState => state.cart;
 
 export const selectIsCartOpen = createSelector(
     [selectCartReducer],
