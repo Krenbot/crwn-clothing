a41fa8b629472f2e65e0c3d10e2eae13a722ab35
Steven Krenn <stevekrenn@gmail.com>
2023-07-20 19:37:02 -0500

    additional nav tests done, dispatch test finished, additonal var init code added

--- src/routes/navigation/__tests__/navigation.component.test.js
+++ src/routes/navigation/__tests__/navigation.component.test.js
@@ -1,8 +1,26 @@
-import { screen } from '@testing-library/react';
+import { screen, fireEvent } from '@testing-library/react';
+import * as reactRedux from 'react-redux';
+
 import Navigation from '../navigation.component';
 import { renderWithProviders } from '../../../utils/test/test.utils';
+import { signOutStart } from '../../../store/user/user.action';
+
+import * as userAction from "../../../store/user/user.action";
+
+jest.mock("react-redux", () => ({
+    ...jest.requireActual("react-redux"),
+    useDispatch: jest.fn(),
+}));
 
 describe('Navigation Tests', () => {
+    const useDispatchMock = reactRedux.useDispatch;
+    beforeEach(() => {
+        useDispatchMock.mockImplementation(() => () => { });
+    });
+    afterEach(() => {
+        useDispatchMock.mockClear();
+    });
+
     test('It should render a sign-in link (and not a sign out link) if there is no current user', () => {
         renderWithProviders(<Navigation />, {
             preloadedState: {
@@ -61,4 +79,24 @@ describe('Navigation Tests', () => {
         const dropdownTextElement = screen.getByText(/your cart is empty/i);
         expect(dropdownTextElement).toBeInTheDocument();
     });
+
+    test('it should dispatch SignOutStart when clicking sign out link', async () => {
+        renderWithProviders(<Navigation />, {
+            preloadedState: {
+                user: {
+                    currentUser: {}
+                }
+            }
+        });
+        const signOutLinkElement = screen.getByText(/sign out/i);
+        //Spy signOutStart
+
+        const signOutStartAction = jest.spyOn(userAction, 'signOutStart');
+        expect(signOutLinkElement).toBeInTheDocument();
+
+        await fireEvent.click(signOutLinkElement);
+        expect(useDispatchMock).toHaveBeenCalled();
+        expect(signOutStartAction).toHaveBeenCalled();
+    });
+
 });
